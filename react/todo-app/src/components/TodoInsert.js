5f74f6c8fad23d11da4878255bd2a50708dfd7fa
young <65516769+jumsoon@users.noreply.github.com>
2020-07-13 17:16:07 +0900

    20200713 삭제기능

--- react/todo-app/src/components/TodoInsert.js
+++ react/todo-app/src/components/TodoInsert.js
@@ -2,15 +2,32 @@ import React, { useState, useCallback } from 'react';
 import { MdCheck } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
     const [value, setValue] = useState('');
 
     const onChange = useCallback(e => {
         setValue(e.target.value);
     },[]);
 
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
+
+    // const onClick = useCallback(
+    //     () => {
+    //         onInsert(value);
+    //         setValue('');
+    //     },
+    //     [onInsert, value],
+    // ); //onkeypress 이벤트가 필요함
+
     return (
-        <form className="TodoInsert">
+        <form className="TodoInsert" onSubmit={onSubmit}>
             <input
                 placeholder="할 일을 입력하세요"
                 value={value}
