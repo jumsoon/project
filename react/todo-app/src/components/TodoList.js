f8d3d3036f3b9a3ef209356ab5bfa12fefe4c7b7
young <65516769+jumsoon@users.noreply.github.com>
2020-07-13 16:02:56 +0900

    20200713

--- react/todo-app/src/components/TodoList.js
+++ react/todo-app/src/components/TodoList.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import TodoListItem from './TodoListltem';
+import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
     return (
         <div className="TodoList">
-            <TodoListItem />
-            <TodoListItem />
-            <TodoListItem />
+            {todos.map(todo => (
+                <TodoListItem todo={todo} key={todo.id} />
+            ))}
         </div>
     );
 };
