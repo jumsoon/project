be494bcf5639b53b24427299183981c5a0b01997
young <65516769+jumsoon@users.noreply.github.com>
2020-07-17 16:40:33 +0900

    20200717 TodoListItem-virtualized

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useCallback} from 'react';
+import React, {useReducer, useRef, useCallback} from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -18,12 +18,23 @@ function createBulkTodos() {
 function todoReducer(todos, action) {
   switch (action.type) {
     case 'INSERT':
-      {type: 'INSERT', todo: { id:1, text:'todo', checked: false } }
+      //{type: 'INSERT', todo: { id:1, text:'todo', checked: false } }
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      //{type:'REMOVE', id: 1}
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      //{type: 'REMOVE', id:1}
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked}:todo,
+        );
+    default:
+      return todos;
   }
 }
 
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
   const nextId = useRef(2501);
 
   //const nextId = useRef(4);
@@ -35,20 +46,16 @@ const App = () => {
         text,
         checked: false,
       };
-      setTodos(todos => todos.concat(todo));
+      dispatch({type: 'INSERT', todo});
       nextId.current += 1;
     },[]);
 
   const onRemove = useCallback(id => {
-      setTodos(todos => todos.filter(todo => todo.id !== id));
+      dispatch({type:'REMOVE', id: 1})
     },[]);
 
   const onToggle = useCallback(id => {
-      setTodos(todos =>
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked } :todo,
-          ),
-      );
+      dispatch({type: 'TOGGLE', id})
     },[]);
 
   return (
