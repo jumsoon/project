f398e82c041426160d7518f95e309ebfdc643f51
young <65516769+jumsoon@users.noreply.github.com>
2020-08-11 14:31:33 +0900

    20200811 main js작업

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -6,5 +6,16 @@ var needPopup = (function() {
     popup.target = 0,
     popup.scrollTopVal = 0,
     popup.scrollHeight = popup.body.scrollHeight > popup.html.scrollHeight ? popup.body.scrollHeight : popup.html.scrollHeight,
-    popup.openHtmlClass = popup.scrollHeight > popup.window.innerHeight ? 'needpopup-opened needpopup-scrolled' : 'need'
+    popup.openHtmlClass = popup.scrollHeight > popup.window.innerHeight ? 'needpopup-opened needpopup-scrolled' : 'needpopup-opened';
+
+    return {
+        init : function() {
+            popup.options = needPopup.config.default;
+            
+            $(popup.body).on('click','[data-needpopup-show]', function(event) {
+				event.preventDefault();
+				needPopup.show($(this).data('needpopupShow'),$(this));
+			})
+        }
+    }
 });
