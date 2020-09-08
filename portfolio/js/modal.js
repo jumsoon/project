3699347707c61dc05718edb4739daca6a4331cf9
jumsoon <anwlgid51@gmail.com>
2020-09-08 09:52:05 +0900

    modal 수정

--- portfolio/js/modal.js
+++ portfolio/js/modal.js
@@ -1,13 +1,99 @@
-document.getElementById("modal-click").onclick = function () {
-    document.getElementById("modal").style.display = "block";
+document.getElementsByClassName("modal-click")[0].onclick = function () {
+    document.getElementsByClassName("modal")[0].style.display = "block";
 }
 
-document.getElementsByClassName("close")[0].onclick = function() {
-    document.getElementById("modal").style.display = "none";
+document.getElementsByClassName("close")[0].onclick = function () {
+    document.getElementsByClassName("modal")[0].style.display = "none";
 }
 
-// window.onclick = function(event) {
-//     if (event.target == document.getElementsById("wrapper")) {
-//         document.getElementById("modal").style.display = "none";
-//     };
-// }
+window.onclick = function(event) {
+    if(event.target == document.getElementsByClassName("modal")[0]) {
+        document.getElementsByClassName("modal")[0].style.display = "none";
+    }
+}
+
+document.getElementsByClassName("modal-click")[1].onclick = function () {
+    document.getElementsByClassName("modal")[1].style.display = "block";
+}
+
+document.getElementsByClassName("close")[1].onclick = function () {
+    document.getElementsByClassName("modal")[1].style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == document.getElementsByClassName("modal")[1]) {
+        document.getElementsByClassName("modal")[1].style.display = "none";
+    }
+}
+
+document.getElementsByClassName("modal-click")[2].onclick = function () {
+    document.getElementsByClassName("modal")[2].style.display = "block";
+}
+
+document.getElementsByClassName("close")[2].onclick = function () {
+    document.getElementsByClassName("modal")[2].style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == document.getElementsByClassName("modal")[2]) {
+        document.getElementsByClassName("modal")[2].style.display = "none";
+    }
+}
+
+document.getElementsByClassName("modal-click")[3].onclick = function () {
+    document.getElementsByClassName("modal")[3].style.display = "block";
+}
+
+document.getElementsByClassName("close")[3].onclick = function () {
+    document.getElementsByClassName("modal")[3].style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == document.getElementsByClassName("modal")[3]) {
+        document.getElementsByClassName("modal")[3].style.display = "none";
+    }
+}
+
+document.getElementsByClassName("modal-click")[4].onclick = function () {
+    document.getElementsByClassName("modal")[4].style.display = "block";
+}
+
+document.getElementsByClassName("close")[4].onclick = function () {
+    document.getElementsByClassName("modal")[4].style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == document.getElementsByClassName("modal")[41]) {
+        document.getElementsByClassName("modal")[4].style.display = "none";
+    }
+}
+
+document.getElementsByClassName("modal-click")[5].onclick = function () {
+    document.getElementsByClassName("modal")[5].style.display = "block";
+}
+
+document.getElementsByClassName("close")[5].onclick = function () {
+    document.getElementsByClassName("modal")[5].style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == document.getElementsByClassName("modal")[5]) {
+        document.getElementsByClassName("modal")[5].style.display = "none";
+    }
+}
+
+document.getElementsByClassName("modal-click")[6].onclick = function () {
+    document.getElementsByClassName("modal")[6].style.display = "block";
+}
+
+document.getElementsByClassName("close")[6].onclick = function () {
+    document.getElementsByClassName("modal")[6].style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == document.getElementsByClassName("modal")[6]) {
+        document.getElementsByClassName("modal")[6].style.display = "none";
+    }
+}
+
+
